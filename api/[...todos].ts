7bd0004f5a2c77c3212061e417fde24585ea3cc7
BISNISCREATIVE <bisniscreative4.0@gmail.com>
2025-09-10 16:22:12 +0700

    Delete api directory

--- api/[...todos].ts
+++
@@ -1,9 +0,0 @@
-import { createServer } from "../server";
-
-// Reuse a single Express instance across invocations
-const app = createServer();
-
-export default function handler(req: any, res: any) {
-  // Delegate to Express (supports both /api/todos and plain /todos via mounted apps)
-  (app as any)(req, res);
-}
