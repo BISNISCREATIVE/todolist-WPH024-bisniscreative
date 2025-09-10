0ad8d39a26f33cb3a3bcda45a3cdd8931e21f4bf
BISNISCREATIVE <bisniscreative4.0@gmail.com>
2025-09-10 16:24:00 +0700

    Delete src directory

--- src/routes/todos/index.ts
+++
@@ -1,5 +0,0 @@
-export { getTodos } from "./getTodos";
-export { getScrollTodos } from "./getScrollTodos";
-export { createTodo } from "./createTodo";
-export { updateTodo } from "./updateTodo";
-export { deleteTodo } from "./deleteTodo";
